7b86bffd24660346c5c189755577b84ed95daa72
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-02-05 18:52:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function EX1() {
+    var frase1= "Um mago não se atrasa, Frodo Bolseiro. Nem se adianta. Ele chega exatamente quando pretende chegar.";
+    var frase2= "Não tenha pena dos mortos, Harry. Tenha pena dos vivos, e acima de tudo, daqueles que vivem sem amor.";
+    var maior;
+    if(frase1.length > frase2.length){
+        maior = frase1
+    }else{
+        maior = frase2
+    }
+    alert(''+ maior)
+}
+
+function EX2() {
+    alert('heeee')
+}
