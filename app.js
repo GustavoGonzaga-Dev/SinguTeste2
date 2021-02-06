87559862fcf936c0156ba4a681756b759eb1e425
gustavo gonzaga <gustavogonzagafarias@gmail.com>
2021-02-05 21:29:25 -0300

    algumas alterações ai

--- app.js
+++ app.js
@@ -11,5 +11,38 @@ function EX1() {
 }
 
 function EX2() {
-    alert('heeee')
+    var frase1= "Um mago não se atrasa, Frodo Bolseiro. Nem se adianta. Ele chega exatamente quando pretende chegar.";
+    var frase2= "I'm going to make him an offer he can't refuse.";
+    var maior;
+    maior = funcao2(frase1, frase2, maior)
+    alert(''+ maior)
+}
+
+function funcao2(frase1, frase2, maior){
+    if(frase1.length > frase2.length){
+        maior = frase1
+    }else{
+        maior = frase2
+    }
+    return maior;
+}
+
+function EX3(){
+    var seuNome="Gustavo"
+    var NomePai="Jose"
+    var NomeMae="Maria"
+    var suaIdade="20"
+    var IdadePai="56"
+    var IdadeMae="50"
+
+    //validação se os campos estão vazios
+
+    var saida = seuNome;
+    saida += "\n" + NomePai;
+    saida += "\n" + NomeMae;
+    saida += "\n" + suaIdade;
+    saida += "\n" + IdadePai;
+    saida += "\n" + IdadeMae
+
+    alert('' + saida)
 }
